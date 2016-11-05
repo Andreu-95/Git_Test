3a2363c2b6bf31f761eac07201624aa119efe87b
Andreu-95 <andreu.g.95@gmail.com>
2016-11-05 12:31:39 -0500

    Primer commit para agregar archivos

---
+++ calc2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <omp.h>
+
+#define dx 0.000001
+
+int main()
+{
+	int i;
+	int t = 1 / dx;
+	double x, f;
+	double pi = 0.0;
+	double init, fin;
+
+	init = omp_get_wtime();
+	#pragma omp parallel for reduction (+:pi) private(x, f)
+	for(i = 0; i < t; i++)
+	{
+		x = (i + 0.5) * dx;
+		f = 4.0 / (1.0 + x * x);
+		//#pragma omp critical
+		pi = pi + f;
+	}
+	fin = omp_get_wtime();
+	
+	pi *= dx;
+	printf("El valor de pi es %.13f, calculado en %fs.\n", pi, fin - init);
+}
+
